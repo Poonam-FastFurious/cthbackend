4f0c5946496dc345852d8b824db81c6946362878
Poonam FastFurious <your-email@example.com>
2024-08-08 14:03:55 +0530

    update io orgin

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
   cors: {
-    origin: "http://localhost:5174",
+    origin: "https://townhall.brandbell.in",
     credentials: true,
   },
 });
