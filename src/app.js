e49c496c9aefd35a50e29f09990cfcfff4efe378
Poonam FastFurious <your-email@example.com>
2024-08-14 15:38:52 +0530

    update api

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
   cors: {
-    origin: "https://townhall.brandbell.in",
+    origin: "http://localhost:5173",
 
     credentials: true,
   },
@@ -15,7 +15,7 @@ const io = new SocketIOServer(server, {
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "https://townhall.brandbell.in",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
   })
 );
